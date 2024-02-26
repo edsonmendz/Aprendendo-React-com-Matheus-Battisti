3ecc87ffdecf5efd7816288d8376db722bd15ed5
edsonmendz <japaact@gmail.com>
2024-02-26 15:43:16 -0300

    Switch atualizado para Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,24 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Empresa from "./pages/Empresa";
 import Contato from "./pages/Contato";
+import Navbar from "./componentes/Navbar";
 
-function App() {
-  const [nome,setNome] = useState()
+function App() {  
 
   return (
     <div className="App">
       <Router>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/empresa'>Empresa</Link>
-          </li>
-          <li>
-            <Link to='/contato'>Contato</Link>
-          </li>
-        </ul>
+        <Navbar />
+        <Routes>          
+          <Route path="/" element={<Home />} > </Route>
+          <Route path="/empresa" element={<Empresa />}></Route>
+          <Route path="/contato" element={<Contato />}></Route>
+        </Routes>
       </Router>
-      <Switch>
-        <Route path="/">
-          <Home></Home>
-        </Route>
-        <Route path="/empresa">
-          <Empresa></Empresa>
-        </Route>
-        <Route path="/">
-          <Contato></Contato>
-        </Route>
-      </Switch>
+      
     </div>
   );
 }
