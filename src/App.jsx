d7e88cf5a3822ef04bb110966c366f36a020611c
edsonmendz <japaact@gmail.com>
2024-02-26 15:22:18 -0300

    atualizando?

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import './App.css';
-import { useState } from 'react';
-import SeuNome from './componentes/SeuNome';
-import Saudacao from './componentes/Saudacao';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Empresa from "./pages/Empresa";
+import Contato from "./pages/Contato";
 
 function App() {
   const [nome,setNome] = useState()
 
   return (
     <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome setNome={setNome}/>
-      <Saudacao nome={nome} />
-      
+      <Router>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/empresa'>Empresa</Link>
+          </li>
+          <li>
+            <Link to='/contato'>Contato</Link>
+          </li>
+        </ul>
+      </Router>
+      <Switch>
+        <Route path="/">
+          <Home></Home>
+        </Route>
+        <Route path="/empresa">
+          <Empresa></Empresa>
+        </Route>
+        <Route path="/">
+          <Contato></Contato>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+            <h1>Principal</h1>
+            <p>Conteúdo da página 1</p>
+        </>
+    )
+}
+
+export default Home
